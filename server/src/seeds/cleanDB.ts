94531c65463e6371041166cca8e11c6cfd6912e1
elimck11 <mrelimcknight@gmail.com>
2024-11-15 11:54:21 -0800

    seeds

--- server/src/seeds/cleanDB.ts
+++ server/src/seeds/cleanDB.ts
@@ -1,10 +1,10 @@
-import { Thought, User } from '../models/index.js';
+import { Joke, User } from '../models/index.js';
 import process from 'process';
 
 const cleanDB = async (): Promise<void> => {
   try {
     // Delete documents from THought collection
-    await Thought.deleteMany({});
+    await Joke.deleteMany({});
     console.log('Thought collection cleaned.');
 
     // Delete documents from User collection
