965985007c6451f3aea7f671d3f7306400d07d23
Stephen Lee <leestephen0320@gmail.com>
2024-11-15 11:27:34 -0800

    JokeBox component added but not finished

---
+++ client/src/components/JokeBox/index.tsx
@@ -0,0 +1,14 @@
+const JokeBox = () => {
+    return (
+      <section className="joke-box">
+        <h2>Joke of the Day</h2>
+        <p className="joke-content">
+          Why did the scarecrow win an award? Because he was outstanding in his
+          field!
+        </p>
+      </section>
+    );
+  };
+  
+  export default JokeBox;
+  
