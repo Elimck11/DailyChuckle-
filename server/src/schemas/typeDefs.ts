a80e4c8236ae98f2b781204ef9df59c4de55012d
Stephen Lee <leestephen0320@gmail.com>
2024-11-18 12:15:03 -0800

    adjusted backend to resolver registerUser

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -49,6 +49,7 @@ const typeDefs = `
 
   type Mutation {
     addUser(input: UserInput!): Auth
+    registerUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     addJoke(input: JokeInput!): Joke
     addComment(jokeId: ID!, commentText: String!): Joke
